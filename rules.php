47f6d69620ed6a2aa54cfed73c6e6ef75cbead58
firecrost <info.xenowix@gmail.com>
2025-05-25 18:06:42 +0300

    Add files via upload

---
+++ rules.php
@@ -0,0 +1,15 @@
+<body>
+<?php include 'templates/navbar.php' ?>
+    <!-- You can change logo at config/config.php -->
+    <div class="container">
+	<div class="rules">
+	<h2><b>Global Rules</b></h2>
+	<li>Cheating is not permitted!</li>
+	<li>Using bugs will result a nice ban</li>
+	<li>More Rules...</li>
+	<br>
+	<h2><b>Another Section</b></h2>
+	<li>Another rules...</li>
+	</div>
+	</div>
+	<?php include 'templates/footer.php' ?>
---
+++ staff.php
@@ -0,0 +1,57 @@
+<body>
+<?php include 'templates/navbar.php' ?>
+<?php
+      // Custom Labels
+      // You can add this here or you can add it to custom.css (It will be active on all pages). It's all about you!
+?>
+<style>.label-test {
+	background-color: #0089FF;
+	color: #fff
+}
+.label-test1 {
+	background-color: #A600DE;
+	color: #fff
+}</style>
+<div class="container">
+    <center>
+<div class="row">
+    <br>
+    <div class="col-md col-md-4">
+        <img class="staff-circle" src="http://cravatar.eu/avatar/ImDinikCZ/100.png">
+        <h2><b>ImDinikCZ</b></h2>
+        <h3><span class="label label-danger"><b>OWNER</b></span></h3>
+    </div>
+        <div class="col-md col-md-4">
+        <img class="staff-circle" src="http://cravatar.eu/avatar/Lordik/100.png">
+        <h2><b>Lordik</b></h2>
+        <h3><span class="label label-success"><b>HELPER</b></span></h3>
+    </div>
+        <div class="col-md col-md-4">
+        <img class="staff-circle" src="http://cravatar.eu/avatar/smajl_CZ/100.png">
+        <h2><b>smajl_CZ</b></h2>
+        <h3><span class="label label-info"><b>BUILDER</b></span></h3>
+    </div>
+	</div>
+	<br>
+	<div class="row">
+            <div class="col-md col-md-4">
+        <img class="staff-circle" src="http://cravatar.eu/avatar/AL1V3M0US3/100.png">
+        <h2><b>AL1V3M0US3</b></h2>
+        <h3><span class="label label-warning"><b>MODERATOR</b></span></h3>
+    </div>
+            <div class="col-md col-md-4">
+        <img class="staff-circle" src="http://cravatar.eu/avatar/187km/100.png">
+        <h2><b>TEST</b></h2>
+        <h3><span class="label label-test"><b>TEST</b></span></h3>
+    </div>
+	            <div class="col-md col-md-4">
+        <img class="staff-circle" src="http://cravatar.eu/avatar/Aciu/100.png">
+        <h2><b>TEST</b></h2>
+        <h3><span class="label label-test1"><b>TEST</b></span></h3>
+    </div>
+    </div>
+        </center>
+		<br>
+		<br>
+</div>
+<?php include 'templates/footer.php' ?>
