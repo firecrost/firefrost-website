0e765b10f8efdc4aee6b5595944b56a381ca036c
firecrost <info.xenowix@gmail.com>
2025-05-25 19:43:54 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const config = {
     serverInfo: {
         serverLogoImageFileName: "logo.png", /*This is a file name for logo in /images/ (If you upload new logo with other name, you must change this value)*/
         serverName: "FİRECROST NETWORK", /*Server name*/
-        serverIp: "FİRECROST NETWORK", /*Server IP (if you want to add online user counter, you must have true the enable-status and enable-query of server.properties)*/
+        serverIp: "firecrost.mcsh.io", /*Server IP (if you want to add online user counter, you must have true the enable-status and enable-query of server.properties)*/
         discordServerID: "489529070913060867" /*Your server ID (if you want to add online user counter, you must have enabled Discord server widget)*/
     },
 
