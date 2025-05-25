c8fac09709311f14f29f059bcea4a99210119911
firecrost <info.xenowix@gmail.com>
2025-05-25 19:55:36 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -181,7 +181,7 @@ const config = {
     Emails are sent via https://formsubmit.co/
     */
     contactPage: {
-        email: "astronavta@example.com"
+        email: "firecrostnetwork@gmail.com"
     }
 }
 
