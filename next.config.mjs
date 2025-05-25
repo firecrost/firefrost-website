f95eeb6c17f7c910f5174897566f3f60ed38882f
firecrost <info.xenowix@gmail.com>
2025-05-25 18:05:04 +0300

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      domains: ['wallpapercave.com' , 'crafatar.com'], // Specify the allowed domain
-    },
-  };
-  
-  export default nextConfig;
-  
